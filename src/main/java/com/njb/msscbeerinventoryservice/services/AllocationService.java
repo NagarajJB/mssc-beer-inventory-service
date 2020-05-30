657458f16eedd82403c14a2a29da2b454a339c7f
Nagaraja Balakrishna <nbalakrishna@manh.com>
2020-05-30 14:20:53 +0530

    AllocationService

---
+++ src/main/java/com/njb/msscbeerinventoryservice/services/AllocationService.java
@@ -0,0 +1,7 @@
+package com.njb.msscbeerinventoryservice.services;
+
+import com.njb.model.BeerOrderDto;
+
+public interface AllocationService {
+	Boolean allocateOrder(BeerOrderDto beerOrderDto);
+}
